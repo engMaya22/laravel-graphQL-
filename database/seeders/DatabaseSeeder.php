84eb348db16306d17fc95c4dc1218d8d61140b80
maya <maya.e@webased.net>
2024-08-21 11:21:25 -0700

    belongs to relation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Post;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -23,6 +24,7 @@ class DatabaseSeeder extends Seeder
              'name' => 'Test User2',
             'email' => 'test@example2.com',
         ]);
+        Post::factory(4)->create();
        
     }
 }
